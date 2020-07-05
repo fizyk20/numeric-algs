878f191ab390b0efb5b693c2984bbfa340be8c33
Bartłomiej Kamiński <fizyk20@gmail.com>
2020-07-05 17:28:31 +0200

    Implement StateDerivative for nalgebra vectors

--- src/traits.rs
+++ src/traits.rs
@@ -22,3 +22,27 @@ pub trait StateDerivative:
 {
     fn abs(&self) -> f64;
 }
+
+#[cfg(feature = "nalgebra")]
+mod nalgebra_impl {
+    use std::ops::{Add, Div, Mul, Neg, Sub};
+
+    use super::StateDerivative;
+
+    use nalgebra::{storage::Storage, Dim, Vector};
+
+    impl<D: Dim, S> StateDerivative for Vector<f64, D, S>
+    where
+        Self: Clone
+            + Add<Self, Output = Self>
+            + Sub<Self, Output = Self>
+            + Mul<f64, Output = Self>
+            + Div<f64, Output = Self>
+            + Neg<Output = Self>,
+        S: Storage<f64, D>,
+    {
+        fn abs(&self) -> f64 {
+            self.dot(self).sqrt()
+        }
+    }
+}
